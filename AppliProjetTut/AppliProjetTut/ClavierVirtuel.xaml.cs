51154379bb18c5dc64c1e33ed6604328afbc4db4
Gaillard <t_of_43@hotmail.fr>
2014-01-14 12:04:11 +0100

    Mise en place de la base (quand meme bien avancée) du projet

---
+++ AppliProjetTut/AppliProjetTut/ClavierVirtuel.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppliProjetTut
+{
+    /// <summary>
+    /// Logique d'interaction pour ClavierVirtuel.xaml
+    /// </summary>
+    public partial class ClavierVirtuel : UserControl
+    {
+        public ClavierVirtuel()
+        {
+            InitializeComponent();
+
+            this.Text.IsEnabled = false;
+
+            this.Cadenas.PreviewTouchDown += new EventHandler<TouchEventArgs>(Cadenas_PreviewTouchDown);
+
+            this.A.PreviewTouchDown += new EventHandler<TouchEventArgs>(A_PreviewTouchDown);
+            this.Z.PreviewTouchDown += new EventHandler<TouchEventArgs>(Z_PreviewTouchDown);
+            this.E.PreviewTouchDown += new EventHandler<TouchEventArgs>(E_PreviewTouchDown);
+        }
+
+        void Cadenas_PreviewTouchDown(object sender, TouchEventArgs e)
+        {
+
+        }
+
+        void E_PreviewTouchDown(object sender, TouchEventArgs e)
+        {
+            this.Text.AppendText("E");
+        }
+
+        void Z_PreviewTouchDown(object sender, TouchEventArgs e)
+        {
+            this.Text.AppendText("Z");
+        }
+
+        void A_PreviewTouchDown(object sender, TouchEventArgs e)
+        {
+            this.Text.AppendText("A");
+        }
+    }
+}
