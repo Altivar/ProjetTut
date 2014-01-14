c495e7d50acabce260613922634725e67a9bc3ac
Gaillard <t_of_43@hotmail.fr>
2014-01-14 16:49:56 +0100

    Mise en place du clavier virtuel par Adrien, mise en place des event d'appui des touches de lettres par MOI !!!

--- AppliProjetTut/AppliProjetTut/ClavierVirtuel.xaml.cs
+++ AppliProjetTut/AppliProjetTut/ClavierVirtuel.xaml.cs
@@ -31,10 +31,33 @@ namespace AppliProjetTut
 
             this.Cadenas.PreviewTouchDown += new EventHandler<TouchEventArgs>(Cadenas_PreviewTouchDown);
 
-            this.A.PreviewTouchDown += new EventHandler<TouchEventArgs>(A_PreviewTouchDown);
-            this.Z.PreviewTouchDown += new EventHandler<TouchEventArgs>(Z_PreviewTouchDown);
-            this.E.PreviewTouchDown += new EventHandler<TouchEventArgs>(E_PreviewTouchDown);
-        }
+            this.A.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.Z.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.E.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.R.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.T.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.Y.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.U.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.I.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.O.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.P.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.Q.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.S.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.D.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.F.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.G.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.H.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.J.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.K.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.L.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.M.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.W.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.X.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.C.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.V.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.B.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+            this.N.PreviewTouchDown += new EventHandler<TouchEventArgs>(OnLetterPreviewTouchDown);
+        }   
 
         void Cadenas_PreviewTouchDown(object sender, TouchEventArgs e)
         {
@@ -44,20 +67,12 @@ namespace AppliProjetTut
             this.CanScale = isMovementEnabled;
         }
 
-        void E_PreviewTouchDown(object sender, TouchEventArgs e)
+        void OnLetterPreviewTouchDown(object sender, TouchEventArgs e)
         {
-            this.Text.AppendText("E");
+            this.Text.AppendText(((SurfaceButton)sender).Content.ToString());
         }
 
-        void Z_PreviewTouchDown(object sender, TouchEventArgs e)
-        {
-            this.Text.AppendText("Z");
-        }
-
-        void A_PreviewTouchDown(object sender, TouchEventArgs e)
-        {
-            this.Text.AppendText("A");
-        }
+        
 
 
 
